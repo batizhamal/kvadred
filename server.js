d68755bb9c46df965f18e9c2c62772f38c3e3c8d
batizhamal <batizhamal@gmail.com>
2025-01-09 18:14:04 +0500

    update package.json

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 const app = express();
 
-// Resolve __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve static files
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Proxy route or API requests
-app.use('/v1', (req, res) => {
-  res.redirect(`https://kvadred.onrender.com${req.url}`);
-});
+app.use(
+  '/v1',
+  createProxyMiddleware({
+    target: 'https://kvadred.onrender.com',
+    changeOrigin: true,
+    pathRewrite: { '^/v1': '' }, // Remove /v1 prefix when forwarding
+  })
+);
 
-// Fallback to index.html for SPA
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
