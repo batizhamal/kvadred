26a73bf88b6a1f84a69abf5c68ad50d30bcbc957
batizhamal <batizhamal@gmail.com>
2025-03-20 11:30:59 +0500

    fix landing block 3

--- src/pages/Landing/partials/Block3/Block3.tsx
+++ src/pages/Landing/partials/Block3/Block3.tsx
@@ -1,25 +1,31 @@
 import './styles.scss';
-import { FaWhatsapp } from 'react-icons/fa6';
+import { FaLink } from 'react-icons/fa6';
 
 function Block3({ className }: { className?: string }) {
   return (
     <div className={`${className}`}>
       <div className={'block-3'}>
-        <div style={{ fontSize: '20px', marginTop: '20px' }}>ABEROY</div>
-        строительная компания
+        {/*<div style={{ fontSize: '20px', marginTop: '20px' }}>ABEROY</div>*/}
+        Участник
         <div className={'title'} style={{ marginTop: '8px' }}>
-          200+ проектов
+          STARTUP GARAGE
         </div>
+        Astana Hub
         <div className={'block-3__circle'}>
-          <img src={'/aberoy.svg'} alt={'logo'} />
+          <img src={'/astanahub.png'} alt={'logo'} />
         </div>
         <div
           className={'block-3__circle--small'}
           onClick={() => {
-            window.open('https://wa.me/+77079883399', '_blank');
+            // window.open('https://wa.me/+77079883399', '_blank');
+            window.open(
+              'https://astanahub.com/en/l/StartupGarage2025',
+              '_blank'
+            );
           }}
         >
-          <FaWhatsapp fill={'#233454'} width={30} height={30} />
+          {/*<FaWhatsapp fill={'#233454'} width={30} height={30} />*/}
+          <FaLink width={20} height={20} fill={'#233454'} />
         </div>
       </div>
     </div>
